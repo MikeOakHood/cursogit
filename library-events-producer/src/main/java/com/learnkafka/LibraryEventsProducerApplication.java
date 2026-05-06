9431471d92cb4a63d242ee014368284e289e8dd6
mrobredo <registros.varios20@gmail.com>
2026-05-06 16:15:03 +0200

    subida tercer camnbio project

--- library-events-producer/src/main/java/com/learnkafka/LibraryEventsProducerApplication.java
+++ library-events-producer/src/main/java/com/learnkafka/LibraryEventsProducerApplication.java
@@ -3,6 +3,11 @@ package com.learnkafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Another change for git course
+ */
+
+
 @SpringBootApplication
 public class   LibraryEventsProducerApplication {
 
